81e15d1811a45a1a91af0108e506b8e392d1e29d
Vaibhav262610 <branda56235@gmail.com>
2022-06-22 05:25:43 -0700

    add

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-// import Hero_section from '../components/Hero_section'
+import Hero_section from '../components/Hero_section'
 // import Navbar from '../components/Navbar'
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-        <div className='flex justify-center items-center h-screen w-full '>
-            <h1>HELLO WORLD</h1>
-        </div>
+        <Hero_section />
     </>
   );
 }
