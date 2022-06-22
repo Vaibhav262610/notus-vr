03372154b03fba57101e8e8a06920b8711623bf1
Vaibhav262610 <branda56235@gmail.com>
2022-06-22 05:05:25 -0700

    Add

---
+++ index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Hero_section from '../components/Hero_section'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero_section />
+    </>
+  )
+}
