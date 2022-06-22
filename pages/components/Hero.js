d571dc90974c03aa46ab0aef7745dec5fe1e88b3
Vaibhav262610 <branda56235@gmail.com>
2022-06-22 05:34:29 -0700

    add

--- pages/components/Hero.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const hero = () => {
-  return (
-    <div><h1>HELLO HERO</h1></div>
-  )
-}
-
-export default hero
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 // import Navbar from '../components/Navbar'
-import Hero from './components/Hero'
+// import Hero from './components/Hero'
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+    <h1>HELLO HERO</h1>
+    <h1>HELLO HERO</h1>
+    <h1>HELLO HERO</h1>
+    <h1>HELLO HERO</h1>
+    <h1>HELLO HERO</h1>
+    <h1>HELLO HERO</h1>
+    <h1>HELLO HERO</h1>
     </>
   );
 }
