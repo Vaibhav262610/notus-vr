caaabaa65b9dd90617fdb5f6295ad5312d7333f4
Vaibhav262610 <branda56235@gmail.com>
2022-06-22 05:51:51 -0700

    add

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Head from "next/head";
-// import { BsFacebook } from "react-icons/bs";
-// import { AiFillTwitterCircle } from "react-icons/ai";
-// import { AiFillGithub } from "react-icons/ai";
-// import { MdDownloadForOffline } from "react-icons/md";
+import { BsFacebook } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+import { MdDownloadForOffline } from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -16,7 +16,7 @@ const Navbar = () => {
           <h1 className="demo text-xs  sm:block hidden cursor-pointer">
             DEMO PAGES
           </h1>
-          {/* <BsFacebook
+          <BsFacebook
             className="fill-[#94a3b8] sm:block hidden cursor-pointer hover:animate-bounce"
             size={20}
           />
@@ -27,10 +27,10 @@ const Navbar = () => {
           <AiFillGithub
             className="fill-[#94a3b8] sm:block hidden cursor-pointer hover:animate-bounce"
             size={24}
-          /> */}
+          />
           <div className="">
             <button className="flex gap-1 items-center transition duration-500 hover:shadow-lg shadow-cyan-500/50 text-sm py-2 rounded-md font-semibold bg-[#334155] px-4 text-white">
-              {/* <MdDownloadForOffline className="fill-white" size={16} /> */}
+              <MdDownloadForOffline className="fill-white" size={16} />
               DOWNLOAD
             </button>
           </div>
