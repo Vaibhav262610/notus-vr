a7f6e7671fba3a423627cb28b64fd8a3bd964681
Vaibhav262610 <branda56235@gmail.com>
2022-06-22 05:32:48 -0700

    add

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 // import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
+import Hero from './components/Hero'
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
